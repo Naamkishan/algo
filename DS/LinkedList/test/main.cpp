aaadf03ed18052861c0a6a78d72421fbd67bc3a0
Abhijit <dev.abhijit@gmail.com>
2017-01-10 07:56:46 +0530

    Simple Singly Linked List implementation

--- DS/LinkedList/test/main.cpp
+++ DS/LinkedList/test/main.cpp
@@ -1,29 +1,148 @@
+#include <stdexcept>
 #include <gtest/gtest.h>
 #include "LinkedList.h"
 
 TEST(TestLinkedList, Initialisation) {
-  ASSERT_NO_THROW(DS::LinkedList<int> llist);
+  ASSERT_NO_THROW(DS::LinkedList<int> list);
 }
 
-#if 0
+TEST(TestLinkedList, ListEmptySize) {
+  DS::LinkedList<int> list;
+  ASSERT_TRUE(list.size() == 0);
+  ASSERT_TRUE(list.empty());
+}
+
+TEST(TestLinkedList, ListUnderflow) {
+  DS::LinkedList<int> list;
+  ASSERT_TRUE(list.size() == 0);
+  ASSERT_TRUE(list.empty());
+  EXPECT_THROW(list.pop_front(), std::out_of_range);
+}
+
 TEST(TestLinkedList, ListPushFront) {
-  LinkList<int> llist;
-  stack.push(10);
-  ASSERT_TRUE(stack.size() == 1);
+  DS::LinkedList<int> list;
+  list.push_front(10);
+  ASSERT_TRUE(list.size() == 1);
 }
 
-TEST(TestLinkedList, StackPop) {
-  Stack<int> stack;
-  stack.push(10);
-  ASSERT_TRUE(stack.pop() == 10);
+TEST(TestLinkedList, ListFront) {
+  DS::LinkedList<int> list;
+  list.push_front(10);
+  ASSERT_TRUE(list.front() == 10);
 }
 
-TEST(TestLinkedList, StackEmpty) {
-  Stack<int> stack;
-  ASSERT_TRUE(stack.empty());
+TEST(TestLinkedList, ListPushBack) {
+  DS::LinkedList<int> list;
+  list.push_back(10);
+  ASSERT_TRUE(list.size() == 1);
 }
 
-#endif
+TEST(TestLinkedList, ListBack) {
+  DS::LinkedList<int> list;
+  list.push_back(10);
+  ASSERT_TRUE(list.back() == 10);
+}
+
+TEST(TestLinkedList, ListFrontBack) {
+  DS::LinkedList<int> list;
+  list.push_front(10);
+  ASSERT_TRUE(list.front() == 10);
+  ASSERT_TRUE(list.back() == 10);
+}
+
+TEST(TestLinkedList, ListPush) {
+  DS::LinkedList<int> list;
+  list.push_back(5);
+  list.push_front(10);
+
+  ASSERT_TRUE(list.back() == 5);
+  ASSERT_TRUE(list.front() == 10);
+  ASSERT_TRUE(list.size() == 2);
+}
+
+TEST(TestLinkedList, ListPopBack) {
+  DS::LinkedList<int> list;
+  list.push_back(5);
+  ASSERT_TRUE(list.back() == 5);
+  list.pop_back();
+  ASSERT_TRUE(list.empty());
+}
+
+TEST(TestLinkedList, ListPopFront) {
+  DS::LinkedList<int> list;
+  list.push_front(5);
+  ASSERT_TRUE(list.front() == 5);
+  list.pop_front();
+  ASSERT_TRUE(list.empty());
+}
+
+TEST(TestLinkedList, ListMultiOpChecks) {
+  DS::LinkedList<int> list;
+  list.push_front(5);
+  ASSERT_TRUE(list.front() == 5);
+  list.pop_front();
+  ASSERT_TRUE(list.empty());
+  list.push_front(4);
+  list.push_front(3);
+  list.push_front(2);
+  list.push_back(5);
+  list.push_back(6);
+  list.push_back(7);
+  list.push_front(1);
+
+  ASSERT_TRUE(list.front() == 1);
+  ASSERT_TRUE(list.back() == 7);
+  ASSERT_TRUE(list.size() == 7);
+  ASSERT_TRUE(list.empty() == false);
+
+  list.pop_back();
+  ASSERT_TRUE(list.front() == 1);
+  ASSERT_TRUE(list.back() == 6);
+  ASSERT_TRUE(list.size() == 6);
+  ASSERT_TRUE(list.empty() == false);
+
+  list.pop_front();
+  ASSERT_TRUE(list.front() == 2);
+  ASSERT_TRUE(list.back() == 6);
+  ASSERT_TRUE(list.size() == 5);
+  ASSERT_TRUE(list.empty() == false);
+
+  list.pop_back();
+  ASSERT_TRUE(list.front() == 2);
+  ASSERT_TRUE(list.back() == 5);
+  ASSERT_TRUE(list.size() == 4);
+  ASSERT_TRUE(list.empty() == false);
+
+  list.push_front(0);
+  ASSERT_TRUE(list.front() == 0);
+  ASSERT_TRUE(list.back() == 5);
+  ASSERT_TRUE(list.size() == 5);
+  ASSERT_TRUE(list.empty() == false);
+
+  list.pop_back();
+  ASSERT_TRUE(list.front() == 2);
+  ASSERT_TRUE(list.back() == 5);
+  ASSERT_TRUE(list.size() == 4);
+  ASSERT_TRUE(list.empty() == false);
+}
+
+TEST(TestLinkedList, ListClear) {
+  DS::LinkedList<int> list;
+  list.push_front(5);
+  list.push_front(4);
+  list.push_front(3);
+  list.push_front(2);
+  list.push_back(6);
+  list.push_back(7);
+  list.push_front(1);
+
+  ASSERT_TRUE(list.size() == 7);
+  ASSERT_TRUE(list.empty() == false);
+
+  list.clear();
+  ASSERT_TRUE(list.empty());
+}
+
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
