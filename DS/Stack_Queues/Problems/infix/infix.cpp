e16fc0e08b072bc7da9d507a5c49da00a79f203f
Abhijit <dev.abhijit@gmail.com>
2017-01-24 21:12:13 +0530

    Basic Implementation

--- DS/Stack_Queues/Problems/infix/infix.cpp
+++ DS/Stack_Queues/Problems/infix/infix.cpp
@@ -1,8 +1,18 @@
+#include <cstdlib>
 #include <iostream>
+#include <string>
+
 #include "dijkstra_infix.h"
+
+using namespace std;
 
 int main() {
   std::cout << "Infix Expression Evaluation!" << std::endl;
 
-  return 0;
+  std::cout << "Evaluating (1 + ( ( 2 + 3 ) * ( 4  * 5 ) ) ): ";
+  std::string expression("(1 +((2+3)*(4*5)))");
+
+  std::cout << dijkstra_infix(expression);
+
+  return EXIT_SUCCESS;
 }
