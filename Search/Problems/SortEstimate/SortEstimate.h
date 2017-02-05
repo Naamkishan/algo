c1f42843d0000605967f7d51fe188dd4f9668d93
Abhijit <dev.abhijit@gmail.com>
2017-02-05 19:39:31 +0530

    SortEstimate.... mostly implemented. TestCase4 fails ... binary search loop enters infinite loops

--- Search/Problems/SortEstimate/SortEstimate.h
+++ Search/Problems/SortEstimate/SortEstimate.h
@@ -7,12 +7,49 @@
 namespace SortEstimate {
 
 bool is_essentially_equal(const double& a, const double& b, const double& tolerance = 1e-9) {
+  double diff = std::fabs(a - b);
   return (std::fabs(a - b) <= tolerance);
 }
 
 
-double how_many(int c, int time) {
-  throw std::logic_error("Function not implemented!");
+/**
+ *
+ * @param constant   - algo's constant
+ * @param numbers    - number of integers to process
+ * @return  double - time taken to process n integers
+ */
+double get_processing_time(const int constant, const double& numbers) {
+  return ( static_cast<double>(constant) * numbers * std::log(numbers)/std::log(2) );
+}
+
+/**
+ * @brief       determines how many integers can be processed for for a given algorithm having O(n): c*n*lg(n)
+ * @param c     constant of the underlying algo's O(n)
+ *                  Range: 1 - 100
+ * @param time  time taken to process "n" integers by the algorithm
+ *                  Range: 1 - 2000000000
+ * @return      double -> number of integers that can be processed
+ */
+double how_many(int c, int time, double tolerance = 1e-9) {
+
+  double numbers{1.00};   // current number of integers being processed
+  double high_num{2000000000};  // max number of integers that can be processed
+  double mid;
+  double processing_time;
+
+  while(!is_essentially_equal(numbers, high_num, tolerance)) {
+    mid = numbers + ((high_num - numbers)/2);
+    processing_time = get_processing_time(c, mid);
+    if(processing_time < time) {
+      numbers = mid;  // can process more integers - we still have some leeway
+    } else {
+      high_num = mid;
+    }
+  }
+
+  return numbers;
+
+
 }
 
 } // namespace SortEstimate
