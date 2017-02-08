ce6affb997ccdaf5cf61c914009dd8ed233b7c19
Abhijit <dev.abhijit@gmail.com>
2017-02-09 05:05:46 +0530

    Updated UnionFind.cpp

--- Search/Methods/UnionFind/UnionFind.cpp
+++ Search/Methods/UnionFind/UnionFind.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 
 
-UnionFind::UnionFind(size_t nodes) {
+UnionFind::UnionFind(std::size_t nodes) {
   container_.reserve(nodes);
   sub_tree_size_.reserve(nodes);
   while(nodes) {
@@ -13,7 +13,7 @@ UnionFind::UnionFind(size_t nodes) {
 }
 
 
-size_t UnionFind::root(size_t i) {
+std::size_t UnionFind::root(std::size_t i) {
   std::size_t root_index = container_[i];
 
   while(container_[root_index] != root_index) {
@@ -25,13 +25,13 @@ size_t UnionFind::root(size_t i) {
 }
 
 
-void UnionFind::set_root(size_t node, size_t root) {
+void UnionFind::set_root(std::size_t node, std::size_t root) {
   container_[node] = root;
   sub_tree_size_[root] += sub_tree_size_[node];
 }
 
 
-void UnionFind::set_union(size_t p, size_t q) {
+void UnionFind::set_union(std::size_t p, std::size_t q) {
   std::size_t root_p = root(p);
   std::size_t root_q = root(q);
 
