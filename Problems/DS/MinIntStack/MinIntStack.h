8c3926f5b395a005afea5a6fda472f0591368f55
Abhijit <dev.abhijit@gmail.com>
2017-02-23 07:49:18 +0530

    Implementation of MinIntStack

---
+++ Problems/DS/MinIntStack/MinIntStack.h
@@ -0,0 +1,74 @@
+#ifndef ALGO_MAXINTSTACK_H
+#define ALGO_MAXINTSTACK_H
+
+#include <stdexcept>
+#include <stack>
+
+namespace algo {
+
+namespace problems {
+
+/**
+ * @brief   MinIntStack: A stack of integers which other than the default stack operations
+ *          also have a min api to show the current min element that is present in the current stack.
+ *
+ *          The max operation has time complexity of O(1) and space complexity of O(1)
+ */
+class MinIntStack {
+ public:
+
+  /**
+   * @brief     Push an element into the stack. Compute the current min element of the stack after push operation
+   * @param value : Value pushed into the stack
+   */
+  void push(int value) {
+    if(stack_.empty()) {
+      cur_min_ = value;
+    } else if(cur_min_ > value) {
+      auto new_elem = (2 * value) - cur_min_;
+      cur_min_ = value;
+      value = std::move(new_elem);
+    }
+    stack_.push(value);
+  }
+
+  /**
+   * @brief     Pops the last entered element of the stack. Throws an exception if stack is empty
+   * @return    int
+   */
+  int pop() {
+    if(stack_.empty())    throw std::runtime_error("Underflow!");
+    auto value = stack_.top();
+    stack_.pop();
+
+    // return the value popped as it is if it was the last element or has value smaller than min
+    if((cur_min_ < value) || stack_.empty())
+      return value;
+
+    // setup the cur min
+    auto elem = cur_min_;
+    cur_min_ = (2 * cur_min_) - value;
+    return elem;
+  }
+
+  /**
+   * @brief     Returns the current min element of the stack. Throws an exception if stack is empty
+   * @return    int
+   */
+  int min() {
+    if(stack_.empty())    throw std::runtime_error("Underflow!");
+    return cur_min_;
+  }
+
+  std::size_t  size() const { return stack_.size(); }
+
+ private:
+  std::stack<int>   stack_;
+  int               cur_min_;
+};
+
+} // namespace algo::problems
+
+} // namespace algo
+
+#endif //ALGO_MAXINTSTACK_H
---
+++ Problems/DS/MinIntStack/Test_MinIntStack.cpp
@@ -0,0 +1,70 @@
+#include <numeric>
+#include <utility>
+
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "MinIntStack.h"
+
+namespace {
+constexpr int NUMBER{100};
+}
+
+
+TEST(Test_MinIntStack, UnderflowCheck) {
+  algo::problems::MinIntStack min_stack;
+
+  EXPECT_THROW(min_stack.pop(), std::runtime_error);
+}
+
+
+TEST(Test_MinIntStack, SizeCheck) {
+  for(std::size_t i{0}; i < NUMBER; ++i) {
+    std::vector<int> input;
+    std::iota(std::begin(input), std::end(input), 1);
+
+    algo::problems::MinIntStack min_stack;
+    for (auto &elem : input) {
+      min_stack.push(elem);
+    }
+
+    EXPECT_EQ(input.size(), min_stack.size());
+  }
+}
+
+TEST(Test_MinIntStack, UnorderedSequenceMaxCheck) {
+  for(std::size_t i{1}; i <= NUMBER; ++i) {
+    std::vector<int> input(i);
+    std::iota(std::begin(input), std::end(input), 1);
+
+    std::random_shuffle(input.begin(), input.end());
+
+    std::vector<int>  stack_values;
+    algo::problems::MinIntStack min_stack;
+    for (auto &elem : input) {
+      min_stack.push(elem);
+      stack_values.push_back(elem);
+    }
+
+    std::vector<int> result;
+    while (min_stack.size()) {
+      auto min_from_stack = min_stack.min();
+      auto min_iter = std::min_element(stack_values.begin(), stack_values.end());
+      EXPECT_EQ(*min_iter, min_from_stack);
+      result.emplace_back(min_stack.pop());
+      stack_values.pop_back();
+    }
+
+    std::reverse(input.begin(), input.end());
+
+    EXPECT_EQ(input, result);
+  }
+}
+
+
+int main(int argc, char** argv) {
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
