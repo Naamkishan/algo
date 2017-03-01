fd2263e199d86d13af4ce5e9f808afb16627c4f1
Abhijit <dev.abhijit@gmail.com>
2017-03-01 23:06:35 +0530

    Find intersection of two linked lists : using both stacks and iteration

--- Problems/DS/LinkedList/SinglyLL/IntersectionPoint/SLL_Intersect_Test.cpp
+++ Problems/DS/LinkedList/SinglyLL/IntersectionPoint/SLL_Intersect_Test.cpp
@@ -163,3 +163,112 @@ TEST_F(SLL_Intersect_Test, CheckIntercepting_SubList) {
   clear(list1);
   clear(list2);
 }
+
+
+TEST_F(SLL_Intersect_Test, CheckEmptyLists_Stack) {
+  DS::detail::Node<int>* list1{nullptr};
+  DS::detail::Node<int>* list2{nullptr};
+
+  DS::detail::Node<int>* pt{nullptr};
+
+  EXPECT_TRUE(algo::problems::get_intersection_point_use_stack(list1, list2) == pt);
+}
+
+TEST_F(SLL_Intersect_Test, CheckNonIntercepting_1Elem_Stack) {
+  DS::detail::Node<int>* list1{nullptr};
+  append_node(list1, new DS::detail::Node<int>(1));
+  DS::detail::Node<int>* list2{nullptr};
+  append_node(list2, new DS::detail::Node<int>(1));
+
+  DS::detail::Node<int>* pt{nullptr};
+
+  EXPECT_TRUE(algo::problems::get_intersection_point_use_stack(list1, list2) == pt);
+
+  clear(list1);
+  clear(list2);
+}
+
+TEST_F(SLL_Intersect_Test, CheckNonIntercepting_Stack) {
+  DS::detail::Node<int>* list1{nullptr};
+  append_node(list1, new DS::detail::Node<int>(1));
+  append_node(list1, new DS::detail::Node<int>(2));
+  append_node(list1, new DS::detail::Node<int>(3));
+  append_node(list1, new DS::detail::Node<int>(4));
+  DS::detail::Node<int>* list2{nullptr};
+  append_node(list2, new DS::detail::Node<int>(1));
+  append_node(list2, new DS::detail::Node<int>(10));
+  append_node(list2, new DS::detail::Node<int>(100));
+  append_node(list2, new DS::detail::Node<int>(1000));
+
+  DS::detail::Node<int>* pt{nullptr};
+
+  EXPECT_TRUE(algo::problems::get_intersection_point_use_stack(list1, list2) == pt);
+
+  clear(list1);
+  clear(list2);
+}
+
+TEST_F(SLL_Intersect_Test, CheckIntercepting_LastNode_Stack) {
+  DS::detail::Node<int>* list1{nullptr};
+  append_node(list1, new DS::detail::Node<int>(1));
+  append_node(list1, new DS::detail::Node<int>(2));
+  append_node(list1, new DS::detail::Node<int>(3));
+  append_node(list1, new DS::detail::Node<int>(4));
+
+  DS::detail::Node<int>* list2{nullptr};
+  append_node(list2, new DS::detail::Node<int>(1));
+  append_node(list2, new DS::detail::Node<int>(10));
+  append_node(list2, new DS::detail::Node<int>(100));
+  append_node(list2, new DS::detail::Node<int>(1000));
+
+  DS::detail::Node<int>* common{new DS::detail::Node<int>(7)};
+  append_node(list1, common);
+  append_node(list2, common);
+
+  EXPECT_TRUE(algo::problems::get_intersection_point_use_stack(list1, list2) == common);
+
+  remove_node(list1, common);
+  remove_node(list2, common);
+  clear(list1);
+  clear(list2);
+  delete common;
+}
+
+TEST_F(SLL_Intersect_Test, CheckIntercepting_FirstNode_Stack) {
+  DS::detail::Node<int>* list1{nullptr};
+  DS::detail::Node<int>* list2{nullptr};
+
+  DS::detail::Node<int>* common{new DS::detail::Node<int>(7)};
+  append_node(list1, common);
+  append_node(list2, common);
+
+  append_node(common, new DS::detail::Node<int>(1));
+  append_node(common, new DS::detail::Node<int>(2));
+  append_node(common, new DS::detail::Node<int>(3));
+  append_node(common, new DS::detail::Node<int>(4));
+
+  EXPECT_TRUE(algo::problems::get_intersection_point_use_stack(list1, list2) == common);
+
+  clear(common);
+}
+
+TEST_F(SLL_Intersect_Test, CheckIntercepting_SubList_Stack) {
+  DS::detail::Node<int>* list1{new DS::detail::Node<int>(7)};
+  append_node(list1, new DS::detail::Node<int>(1));
+  append_node(list1, new DS::detail::Node<int>(2));
+  append_node(list1, new DS::detail::Node<int>(3));
+  append_node(list1, new DS::detail::Node<int>(4));
+
+  DS::detail::Node<int>* list2{nullptr};
+  append_node(list2, new DS::detail::Node<int>(1));
+  append_node(list2, new DS::detail::Node<int>(10));
+  append_node(list2, new DS::detail::Node<int>(100));
+  append_node(list2, new DS::detail::Node<int>(1000));
+  append_node(list2, list1);
+
+  EXPECT_TRUE(algo::problems::get_intersection_point_use_stack(list1, list2) == list1);
+
+  end_list_at_node(list2, list1);
+  clear(list1);
+  clear(list2);
+}
--- Problems/DS/LinkedList/SinglyLL/IntersectionPoint/sll_instersect.h
+++ Problems/DS/LinkedList/SinglyLL/IntersectionPoint/sll_instersect.h
@@ -9,8 +9,14 @@ namespace algo {
 
 namespace problems {
 
+/**
+ * @brief   Use stack. If the lists intersects, the last node has to be same for both. Order : O(n + m)
+ * @param left
+ * @param right
+ * @return  address of the point of intersection or nullptr if none
+ */
 template<typename T>
-DS::detail::Node<T>* get_intersection_point(DS::detail::Node<T>* left, DS::detail::Node<T>* right) {
+DS::detail::Node<T>* get_intersection_point_use_stack(DS::detail::Node<T>* left, DS::detail::Node<T>* right) {
   using NodeStack = DS::Stack<DS::detail::Node<T>*>;
   NodeStack left_stack, right_stack;
 
@@ -28,6 +34,50 @@ DS::detail::Node<T>* get_intersection_point(DS::detail::Node<T>* left, DS::detai
   }
 
   return pt;
+}
+
+
+template<typename T>
+std::size_t list_size(DS::detail::Node<T>* list) {
+  std::size_t size = 0;
+  for(; list; list = list->next_)
+    ++size;
+  return size;
+}
+
+/**
+ * @brief   get the intersection point of two lists by checking the last possible common nodes
+ * @param left
+ * @param right
+ * @param diff  : difference of length of two lists
+ * @return
+ */
+template<typename T>
+DS::detail::Node<T>* get_intersection_point(DS::detail::Node<T>* left, DS::detail::Node<T>* right) {
+  if(left && right) {
+    int diff = list_size(left) - list_size(right);
+
+    DS::detail::Node<T>** head = (diff > 0) ? &left : &right;
+    diff = abs(diff);
+
+    while(diff) {
+      *head = (*head)->next_;
+      --diff;
+    }
+
+    // now left and right are in sync wrt to last "size - diff" positions
+    while(left && right) {
+      if(left == right)
+        return left;
+
+      // update the pointers
+      left = left->next_;
+      right = right->next_;
+    }
+
+  }
+
+  return nullptr;
 }
 
 } // namespace algo::problems
