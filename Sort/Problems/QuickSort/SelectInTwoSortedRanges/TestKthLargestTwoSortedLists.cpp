fd0c3af7e5c69508949729813d5b5bc0fc894845
Abhijit <dev.abhijit@gmail.com>
2017-04-10 01:56:22 +0530

    nth_element: two sorted lists: logrithmic order

--- Sort/Problems/QuickSort/SelectInTwoSortedRanges/TestKthLargestTwoSortedLists.cpp
+++ Sort/Problems/QuickSort/SelectInTwoSortedRanges/TestKthLargestTwoSortedLists.cpp
@@ -11,5 +11,43 @@ TEST(KthLargest, LinearOrder) {
 
   using namespace algo::problems::nth_element::multilist;
 
-  EXPECT_EQ(nth_element_2list_linear(primes.begin(), primes.end(), squares.begin(), squares.end(), 4), 5);
+  EXPECT_EQ(nth_element_2list_linear(primes.begin(), primes.end(), squares.begin(), squares.end(), 2), 25);
+}
+
+TEST(KthLargest, LogarithmicOrder_2) {
+  std::vector<int>  primes{2, 3, 5, 7, 11};
+  std::vector<int>  squares{4, 9, 16, 25, 36};
+
+  using namespace algo::problems::nth_element::multilist;
+
+  EXPECT_EQ(nth_element_2list_logarithmic(primes.begin(), primes.end(), squares.begin(), squares.end(), 2), 25);
+}
+
+TEST(KthLargest, LogarithmicOrder_1) {
+  std::vector<int>  primes{2, 3, 5, 7, 11};
+  std::vector<int>  squares{4, 9, 16, 25, 36};
+
+  using namespace algo::problems::nth_element::multilist;
+
+  EXPECT_EQ(nth_element_2list_logarithmic(primes.begin(), primes.end(), squares.begin(), squares.end(), 1), 36);
+}
+
+TEST(KthLargest, LogarithmicOrder_10) {
+  std::vector<int>  primes{2, 3, 5, 7, 11};
+  std::vector<int>  squares{4, 9, 16, 25, 36};
+
+  using namespace algo::problems::nth_element::multilist;
+
+  EXPECT_EQ(nth_element_2list_logarithmic(primes.begin(), primes.end(), squares.begin(), squares.end(), 10), 2);
+}
+
+TEST(KthLargest, LogarithmicOrder_OneEmpty) {
+  std::vector<int>  primes{};
+  std::vector<int>  squares{1, 2, 3, 4, 9, 16, 25, 36};
+
+  using namespace algo::problems::nth_element::multilist;
+
+  EXPECT_EQ(nth_element_2list_logarithmic(primes.begin(), primes.end(), squares.begin(), squares.end(), 8), 1);
+  EXPECT_EQ(nth_element_2list_logarithmic(primes.begin(), primes.end(), squares.begin(), squares.end(), 1), 36);
+  EXPECT_EQ(nth_element_2list_logarithmic(primes.begin(), primes.end(), squares.begin(), squares.end(), 4), 9);
 }
--- Sort/Problems/QuickSort/SelectInTwoSortedRanges/kth_largest_multilist.h
+++ Sort/Problems/QuickSort/SelectInTwoSortedRanges/kth_largest_multilist.h
@@ -3,6 +3,7 @@
 
 #include <iterator>
 #include <numeric>
+#include <stdexcept>
 #include <utility>
 #include <vector>
 
@@ -15,7 +16,7 @@ namespace nth_element {
 namespace multilist {
 
 
-#if 0
+#if TODO // for multiple lists
 template<typename RandomIterator>
 using Range = std::pair<RandomIterator, RandomIterator>;
 
@@ -44,43 +45,12 @@ auto nth_element_sorted(ForwardIterator begin, ForwardIterator end, Comparator c
   );
 }
 
-
-/**
- * @brief   Finds the Nth element of the sorted lists in logarithmic time
- * @tparam ForwardIterator
- * @tparam Comparator
- * @param start_1
- * @param end_1
- * @param start_2
- * @param end_2
- * @param nth_index
- * @param comparator
- * @return  the value of the nth_element
- */
-template<typename ForwardIterator,
-    typename Comparator = std::less<typename std::iterator_traits<ForwardIterator>::value_type>
->
-auto nth_element_2list_logarithmic(ForwardIterator start_1,
-                              ForwardIterator end_1,
-                              ForwardIterator start_2,
-                              ForwardIterator end_2,
-                              std::size_t nth_index,
-                              Comparator comparator = Comparator()
-) {
-  auto elem_idx = 0;
-  ForwardIterator nth_element;
-
-
-
-  return *nth_element;
-}
-
 #endif
 
 
 /**
  * @brief   Finds the Nth element of the sorted lists in linear time
- * @tparam ForwardIterator
+ * @tparam BiIterator
  * @tparam Comparator
  * @param start_1
  * @param end_1
@@ -90,35 +60,119 @@ auto nth_element_2list_logarithmic(ForwardIterator start_1,
  * @param comparator
  * @return  the value of the nth_element
  */
-template<typename ForwardIterator,
-         typename Comparator = std::less<typename std::iterator_traits<ForwardIterator>::value_type>
+template<typename BiIterator,
+         typename Comparator = std::greater<typename std::iterator_traits<BiIterator>::value_type>
 >
-auto nth_element_2list_linear(ForwardIterator start_1,
-                              ForwardIterator end_1,
-                              ForwardIterator start_2,
-                              ForwardIterator end_2,
+auto nth_element_2list_linear(BiIterator begin_1,
+                              BiIterator end_1,
+                              BiIterator begin_2,
+                              BiIterator end_2,
                               std::size_t nth_index,
                               Comparator comparator = Comparator()
 ) {
   auto elem_idx = 0;
-  ForwardIterator nth_element;
+  std::reverse_iterator<BiIterator> nth_element;
 
-  while((start_1 != end_1) && (start_2 != end_2)) {
+  // convert the sequences into descending order ones
+  std::reverse_iterator<BiIterator> start_1(end_1);
+  std::reverse_iterator<BiIterator> finish_1(begin_1);
+  std::reverse_iterator<BiIterator> start_2(end_2);
+  std::reverse_iterator<BiIterator> finish_2(begin_2);
+
+  while((start_1 != finish_1) && (start_2 != finish_2)) {
     nth_element = comparator(*start_1, *start_2) ? start_1++ : start_2++;
     if(++elem_idx == nth_index)
       break;
   }
 
 
-  while((elem_idx != nth_index) && (start_1 != end_1)) {
+  while((elem_idx != nth_index) && (start_1 != finish_1)) {
     nth_element = ++start_1;
   }
 
-  while((elem_idx != nth_index) && (start_2 != end_2)) {
+  while((elem_idx != nth_index) && (start_2 != finish_2)) {
     nth_element = ++start_1;
   }
 
   return *nth_element;
+}
+
+
+/**
+ * @brief   Finds the Nth element of the sorted lists in logarithmic time
+ * @tparam RandomIterator
+ * @tparam Comparator
+ * @param start_1
+ * @param end_1
+ * @param start_2
+ * @param end_2
+ * @param nth_index
+ * @param comparator
+ * @return  the value of the nth_element
+ */
+template<typename RandomIterator,
+    typename Comparator = std::greater<typename std::iterator_traits<RandomIterator>::value_type>
+>
+auto nth_element_2list_logarithmic(RandomIterator start_1,
+                                   RandomIterator end_1,
+                                   RandomIterator start_2,
+                                   RandomIterator end_2,
+                                   std::size_t nth_index,
+                                   Comparator comparator = Comparator()
+) {
+  --nth_index;  //  correction: our comparison is 0-based
+
+  // converting the list from ascending to descending order
+  std::reverse_iterator<RandomIterator> rstart_1(end_1);
+  std::reverse_iterator<RandomIterator> rend_1(start_1);
+  std::reverse_iterator<RandomIterator> rstart_2(end_2);
+  std::reverse_iterator<RandomIterator> rend_2(start_2);
+
+  // store the heads of the reversed lists
+  std::reverse_iterator<RandomIterator> rhead_1{rstart_1};
+  std::reverse_iterator<RandomIterator> rhead_2{rstart_2};
+
+
+  auto difference = 0;
+
+  // loop as long as both of the lists are traversable
+  while((rstart_1 != rend_1) &&
+      (rstart_2 != rend_2)) {
+
+    difference = nth_index - (std::distance(rhead_1, rstart_1) + std::distance(rhead_2, rstart_2));
+
+    if(difference > 0) { // move upwards the list (the lower range)
+      if(comparator(*rstart_1, *rstart_2)) {
+        std::advance(++rstart_1, std::distance(rstart_1, rend_1) >> 1);
+      } else {
+        std::advance(++rstart_2, std::distance(rstart_2, rend_2) >> 1);
+      }
+    } else if(difference < 0) { // move downwards the list (the higher range)
+      if(comparator(*rstart_1, *rstart_2)) {
+        // can we move
+        if(rstart_2 != rhead_2)
+          std::advance(--rstart_2, -std::distance(rstart_2, rend_2) >> 1);
+        else
+          rend_2 = rstart_2;
+      } else {
+        // check if we can move forward
+        if(rstart_1 != rhead_1)
+          std::advance(--rstart_1, -std::distance(rstart_1, rend_1) >> 1);
+        else
+          rend_1 = rstart_1; // we have exhausted the list
+      }
+    } else {
+      // we got the index which points to the nth-largest element
+      return comparator(*rstart_1, *rstart_2) ? *rstart_1 : *rstart_2;
+    }
+  }
+
+  // one of the lists have exhausted
+  std::reverse_iterator<RandomIterator> active = (rstart_1 != rend_1) ? rstart_1 : rstart_2; // there are only two lists .. so checking emptiness for one should suffice
+
+  difference = nth_index - (std::distance(rhead_1, rstart_1) + std::distance(rhead_2, rstart_2));
+
+  return *std::next(active, difference);
 }
 
 
